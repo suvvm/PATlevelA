ebaa8f51ac48853c2fd7ce8a7656bd462ed2bc89
suvvm <41322877+suvvm@users.noreply.github.com>
2018-09-15 16:02:27 +0800

    1001

---
+++ 1001.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <ctype.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b;
+    c = a + b;
+    char d[20];
+    sprintf(d, "%d", c);
+
+    cout << d;
+    return 0;
+}
