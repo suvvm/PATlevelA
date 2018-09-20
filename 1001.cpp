68c9564c9191280fe8aa254c5d27fda2a72b7aa7
suvvm <41322877+suvvm@users.noreply.github.com>
2018-09-20 21:51:45 +0800

    1001

--- 1001.cpp
+++ 1001.cpp
@@ -1,15 +1,33 @@
 #include <bits/stdc++.h>
-#include <ctype.h>
 using namespace std;
-
+typedef long long LL;
 int main()
 {
-    int a, b, c;
+    vector<char> v;
+    LL a, b, c, cnt = 0;
+    char temp;
     cin >> a >> b;
     c = a + b;
-    char d[20];
-    sprintf(d, "%d", c);
+    if(c < 0){
+        cout << "-";
+        c = -c;
+    }
+    if(c == 0)
+        cout << 0;
+    while(c){
 
-    cout << d;
+        temp = c % 10 + '0';
+        v.push_back(temp);
+        c /= 10;
+        cnt++;
+        if(cnt == 3 && c != 0){
+            v.push_back(',');
+            cnt = 0;
+        }
+    }
+    reverse(v.begin(), v.end());
+    for(auto it : v){
+        cout << it;
+    }
     return 0;
 }
